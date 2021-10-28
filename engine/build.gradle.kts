5449ae89147f1c863ffbc652fde25b735735b19e
Jean d'Arc <darc.zynaps@gmail.com>
2021-10-28 13:37:19 +0100

    Upgraded test dependencies

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -2,8 +2,8 @@ repositories { mavenCentral() }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.7.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.assertj:assertj-core:3.21.0")
 }
 
 tasks.test { useJUnitPlatform() }
